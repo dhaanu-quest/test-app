e5b8c5487d735707bb39e5e9ab716cbc3ec2583e
dhaanuofficial <dhaanu.i@questlabs.biz>
2024-04-15 23:17:19 +0530

    aa

--- app.js
+++ app.js
@@ -8,16 +8,43 @@ app.use(express.json())
 
 const web = new WebClient(process.env.BOT_TOKEN);
 
-app.post('/test', async (req, res) => { 
+const randomResponses = [
+    "I'm doing great, thanks for asking!",
+    "Not bad, how about you?",
+    "Feeling awesome today!",
+    "Just chilling, you?",
+    "Pretty good, thanks!",
+    "Could be better, but hanging in there!",
+    "Not too shabby!",
+    "Living the dream!",
+];
+
+
+function getRandomResponse() {
+    return randomResponses[Math.floor(Math.random() * randomResponses.length)];
+}
+
+
+app.post('/test', async (req, res) => {
     console.log("testSlackFunction");
     try {
         console.log(req?.body)
         console.log(req?.body?.event?.text);
-        // const result = await web.chat.postMessage({
-        //     channel: req.body.event.channel,
-        //     text: "How are you",
-        // });
-        // console.log(result); 
+        const eventData = req.body;
+
+        if (eventData.type === 'event_callback' && eventData.event && eventData.event.type === 'message' && !eventData.event.bot_id) {
+            console.log("Received user message:", eventData.event.text);
+
+            const randomMessage = getRandomResponse();
+
+            const result = await web.chat.postMessage({
+                channel: eventData.event.channel,
+                text: randomMessage,
+            });
+            console.log("Sent reply to user message:", result);
+        } else if (eventData.ok === true && eventData.message && eventData.message.bot_id) {
+            console.log("Received bot reply:", eventData.message.text);
+        }
         res.json({ challenge: req?.body?.challenge });
     } catch (error) {
         console.log(error);
